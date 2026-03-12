45550b381ad659f1092e79fed11310ed5c04950c
bylogli27 <hugo.ruiz@uma.es>
2026-03-12 10:13:05 +0100

    update

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -17,9 +17,14 @@ import java.text.SimpleDateFormat;
  */
 public class Date {
   private final int day;
+
   private final int month;
+
   private final int year;
 
+  public void methodAddedByOwner() {
+    System.out.println("Method added by the owner");
+  }
   /**
    * Retrieves the day component of this date.
    *
